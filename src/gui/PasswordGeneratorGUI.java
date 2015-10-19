90d6407cb95df273dabfdbdcf4796b6ae49364c8
RahulSriram <rahulrockingsriram@gmail.com>
2015-10-19 22:21:56 +0530

    minor error fixes - v1.1

--- src/gui/PasswordGeneratorGUI.java
+++ src/gui/PasswordGeneratorGUI.java
@@ -7,7 +7,7 @@ import java.util.Random;
  */
 public class PasswordGeneratorGUI extends javax.swing.JFrame {
 
-	public mainUI() {
+	public PasswordGeneratorGUI() {
 		initComponents();
 	}
 
@@ -202,18 +202,18 @@ public class PasswordGeneratorGUI extends javax.swing.JFrame {
 		try {
 			javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
 		} catch (ClassNotFoundException ex) {
-			java.util.logging.Logger.getLogger(mainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			java.util.logging.Logger.getLogger(PasswordGeneratorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (InstantiationException ex) {
-			java.util.logging.Logger.getLogger(mainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			java.util.logging.Logger.getLogger(PasswordGeneratorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (IllegalAccessException ex) {
-			java.util.logging.Logger.getLogger(mainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			java.util.logging.Logger.getLogger(PasswordGeneratorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-			java.util.logging.Logger.getLogger(mainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			java.util.logging.Logger.getLogger(PasswordGeneratorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		}
 
 		java.awt.EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				new mainUI().setVisible(true);
+				new PasswordGeneratorGUI().setVisible(true);
 			}
 		});
 	}
